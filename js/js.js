442a976e3af47b538212df219c86fbc2eeb888c2
Michael <Michael.hao25@gmail.com>
2018-03-20 13:30:05 +0800

    add swiper plugs

--- js/js.js
+++ js/js.js
@@ -10,4 +10,23 @@ define(['jquery'], function($) {
 			window.location.href = $(this).attr('href');
 		}
 	});
+
+	$('*[data-js-tabs]').children().on('click', function() {
+		var tabs_obj = $(this).parent().attr('data-js-tabs');
+		$(tabs_obj).children().eq($(this).index()).show().siblings().hide();
+		$(this).addClass('active').siblings().removeClass('active');
+	});
+	$.each($('*[data-js-tabs]'), function(index, el) {
+		var arg = window.location.search;
+		if (arg != '') {
+			arg = arg.split('?')[1].split('=')[1];
+			console.log(arg);
+			$(el).children().eq(arg - 1).trigger('click');
+
+		} else {
+			$(el).children().first().addClass('active');
+			$($(el).attr('data-js-tabs')).children().eq(0).show().siblings().hide();;
+		}
+	});
+
 })
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ require.config({
 	paths: {
 		'jquery': "jquery",
 		//jquery默认加载
+		'swiper': 'swiper',
 		"js": "js"
 	}
 })
